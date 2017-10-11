ace1b3192f4244fa479f09e0d8b2a48a98fa7432
andyWqh <andyWqh@163.com>
2017-10-11 17:39:05 +0800

    实现酷炫聊天室功能

---
+++ SignaIRProject/SignaIRWPFClient/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace SignaIRWPFClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly char[] Constant =
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z'
+        };
+
+        public IHubProxy HubProxy { get; set; }
+        const string serverUrl = "http://localhost:8090/signair";
+        public HubConnection Connection { get; set; }
+        public MainWindow()
+        {
+            //初始化窗口控件
+            InitializeComponent();
+            //启动窗口时开始连接服务器
+        }
+    }
+}
