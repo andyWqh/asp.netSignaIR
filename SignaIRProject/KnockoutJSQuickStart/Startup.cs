9566caf6e53c755c6833cbc120bca7e802f1b5e4
andyWqh <andyWqh@163.com>
2017-10-16 17:09:57 +0800

    KnockoutJs简单入门

---
+++ SignaIRProject/KnockoutJSQuickStart/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnockoutJSQuickStart.Startup))]
+namespace KnockoutJSQuickStart
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
